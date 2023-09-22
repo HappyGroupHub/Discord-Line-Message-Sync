03eab0850d15891ba4e7d318bce67daa14bfb6f4
LD <chiyc88@gmail.com>
2023-09-23 02:34:29 +0800

    Optimize - optimize codes in line_bot.py

--- line_bot.py
+++ line_bot.py
@@ -54,19 +54,19 @@ def handle_message(event):
     if event.source.type == 'user':
         return
     if event.source.type == 'group':
-        if event.message.text == '!ID':
-            line_bot_api.reply_message(
-                event.reply_token,
-                TextSendMessage(text=event.source.group_id))
-        if event.source.group_id in config.get('subscribed_line_channels'):
-            sub_num = config.get('subscribed_line_channels').index(event.source.group_id) + 1
-            author = line_bot_api.get_group_member_profile(event.source.group_id,
-                                                           event.source.user_id).display_name
-            author_image = line_bot_api.get_group_member_profile(event.source.group_id,
-                                                                 event.source.user_id).picture_url
-            message = event.message.text
-            discord_webhook = SyncWebhook.from_url(config.get(f'discord_channel_webhook_{sub_num}'))
-            discord_webhook.send(message, username=f"{author} - (Line訊息)", avatar_url=author_image)
+        message_received = event.message.text
+        user_id = event.source.user_id
+        group_id = event.source.group_id
+        reply_token = event.reply_token
+        if message_received == '!ID':
+            line_bot_api.reply_message(reply_token, TextSendMessage(text=group_id))
+        if group_id in config['subscribed_line_channels']:
+            sub_num = config['subscribed_line_channels'].index(group_id) + 1
+            author = line_bot_api.get_group_member_profile(group_id, user_id).display_name
+            author_image = line_bot_api.get_group_member_profile(group_id, user_id).picture_url
+            discord_webhook = SyncWebhook.from_url(config[f'discord_channel_webhook_{sub_num}'])
+            discord_webhook.send(message_received, username=f"{author} - (Line訊息)",
+                                 avatar_url=author_image)
 
 
 @handler.add(MessageEvent, message=ImageMessage)
@@ -75,15 +75,15 @@ def handle_image(event):
     if event.source.type == 'user':
         return
     if event.source.type == 'group':
-        if event.source.group_id in config.get('subscribed_line_channels'):
-            sub_num = config.get('subscribed_line_channels').index(event.source.group_id) + 1
-            author = line_bot_api.get_group_member_profile(event.source.group_id,
-                                                           event.source.user_id).display_name
-            author_image = line_bot_api.get_group_member_profile(event.source.group_id,
-                                                                 event.source.user_id).picture_url
+        user_id = event.source.user_id
+        group_id = event.source.group_id
+        if group_id in config['subscribed_line_channels']:
+            sub_num = config['subscribed_line_channels'].index(group_id) + 1
+            author = line_bot_api.get_group_member_profile(group_id, user_id).display_name
+            author_image = line_bot_api.get_group_member_profile(group_id, user_id).picture_url
             source = line_bot_api.get_message_content(event.message.id)
             file_path = utils.download_file_from_line(sub_num, source, event.message.type)
-            discord_webhook = SyncWebhook.from_url(config.get(f'discord_channel_webhook_{sub_num}'))
+            discord_webhook = SyncWebhook.from_url(config[f'discord_channel_webhook_{sub_num}'])
             discord_webhook.send(file=File(file_path), username=f"{author} - (Line訊息)",
                                  avatar_url=author_image)
 
@@ -94,15 +94,15 @@ def handle_video(event):
     if event.source.type == 'user':
         return
     if event.source.type == 'group':
-        if event.source.group_id in config.get('subscribed_line_channels'):
-            sub_num = config.get('subscribed_line_channels').index(event.source.group_id) + 1
-            author = line_bot_api.get_group_member_profile(event.source.group_id,
-                                                           event.source.user_id).display_name
-            author_image = line_bot_api.get_group_member_profile(event.source.group_id,
-                                                                 event.source.user_id).picture_url
+        user_id = event.source.user_id
+        group_id = event.source.group_id
+        if group_id in config['subscribed_line_channels']:
+            sub_num = config['subscribed_line_channels'].index(group_id) + 1
+            author = line_bot_api.get_group_member_profile(group_id, user_id).display_name
+            author_image = line_bot_api.get_group_member_profile(group_id, user_id).picture_url
             source = line_bot_api.get_message_content(event.message.id)
             file_path = utils.download_file_from_line(sub_num, source, event.message.type)
-            discord_webhook = SyncWebhook.from_url(config.get(f'discord_channel_webhook_{sub_num}'))
+            discord_webhook = SyncWebhook.from_url(config[f'discord_channel_webhook_{sub_num}'])
             discord_webhook.send(file=File(file_path), username=f"{author} - (Line訊息)",
                                  avatar_url=author_image)
 
@@ -113,15 +113,15 @@ def handle_audio(event):
     if event.source.type == 'user':
         return
     if event.source.type == 'group':
-        if event.source.group_id in config.get('subscribed_line_channels'):
-            sub_num = config.get('subscribed_line_channels').index(event.source.group_id) + 1
-            author = line_bot_api.get_group_member_profile(event.source.group_id,
-                                                           event.source.user_id).display_name
-            author_image = line_bot_api.get_group_member_profile(event.source.group_id,
-                                                                 event.source.user_id).picture_url
+        user_id = event.source.user_id
+        group_id = event.source.group_id
+        if group_id in config['subscribed_line_channels']:
+            sub_num = config['subscribed_line_channels'].index(group_id) + 1
+            author = line_bot_api.get_group_member_profile(group_id, user_id).display_name
+            author_image = line_bot_api.get_group_member_profile(group_id, user_id).picture_url
             source = line_bot_api.get_message_content(event.message.id)
             file_path = utils.download_file_from_line(sub_num, source, event.message.type)
-            discord_webhook = SyncWebhook.from_url(config.get(f'discord_channel_webhook_{sub_num}'))
+            discord_webhook = SyncWebhook.from_url(config[f'discord_channel_webhook_{sub_num}'])
             discord_webhook.send(file=File(file_path), username=f"{author} - (Line訊息)",
                                  avatar_url=author_image)
 
